ecde9ab9c2aa2c912b5ad08f77ccdabf66752d46
manoVaturi <ormanor@gmail.com>
2022-11-17 16:36:12 +0200

    Configure docker compose and git checks for waiting db and migrate

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'django-insecure-1!n$cb0^3a0d&wck+bz3h$c0br31qz*k-jwj_e_yp3n#3)$aq-'
+# SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

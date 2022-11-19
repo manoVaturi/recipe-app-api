6ec88117e0f2cc00087dc6d6f9c3c7b4220c41dc
manoVaturi <ormanor@gmail.com>
2022-11-19 20:21:32 +0200

    Added user create tests and view fix lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -41,8 +41,7 @@ class ModelTest(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser
-        ('test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser('t@e.com', '123')
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)

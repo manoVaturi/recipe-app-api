d44bbc30160da9e5529e45c33b5357a1cabe9b84
manoVaturi <ormanor@gmail.com>
2022-11-19 20:17:39 +0200

    Added user create tests and view - fix lint

--- app/app/urls.py
+++ app/app/urls.py
@@ -6,9 +6,9 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
-    path('admin/', admin.site.urls),
     path('api/user/', include('user.urls'))
 ]
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -41,7 +41,8 @@ class ModelTest(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser
+        ('test@example.com', 'test123')
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
